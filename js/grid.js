c02d4bb313451c4fe5105885f68c44b17fd98bff
4l3x <alexander.auermaa@gmail.com>
2022-06-25 13:59:20 +0200

    4k 5 letter words added

--- js/grid.js
+++ js/grid.js
@@ -3,28 +3,16 @@ const cols = 5;
 
 function spawnGrid() {
     for (let index = 0; index < 30; index++) {
-        let rowNumber = index + 1;
-        spawnTile(rowNumber);
+        spawnTile();
     }
     currentInput();
 }
 
-function spawnRow(rowNumber) {
-        const grid = document.querySelector(".grid");
-        const row = document.createElement('div');
-        row.classList.add("row" + rowNumber, "grid--rows");
-        grid.append(row);
-        for (let index = 0; index < cols; index++) {
-            spawnTile(rowNumber);
-        }
-    }
-
-function spawnTile(rowNumber) {
+function spawnTile() {
     const tile = document.createElement('div');
     tile.classList.add("tile");
     const row = document.querySelector(".grid");
     row.append(tile);
-    console.log(rowNumber);
 }
 
 
--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ document.addEventListener("DOMContentLoaded", main);
 function main() {
     spawnGrid();
     createKeyboard();
+    printRandomWord();
+}
+
+function printRandomWord() {
+    const randomNumber = Math.floor(Math.random() * words.length);
+    console.log(words[randomNumber]);
 }
