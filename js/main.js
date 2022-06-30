4a443ea072cc2ff64fef4944bd922ba79b964ea5
4l3x <alexander.auermaa@gmail.com>
2022-06-30 16:07:18 +0200

    how-to now only displays if sessionStorage is empty

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", main);
+var hasVisited = sessionStorage.getItem("ordentlig.se");
 
 function main() {
+  spawnGrid();
+  createKeyboard();
+  gameLoop();
+
+  if (!hasVisited) {
     openInfo();
-    spawnGrid();
-    createKeyboard();
-    gameLoop();
+    sessionStorage.setItem("ordentlig.se", true);
+  }
 }
